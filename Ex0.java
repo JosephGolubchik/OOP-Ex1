90d9c67c2badb208793fd3337ef001f3c2906564
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-11-04 10:45:41 +0200

    Add files via upload

---
+++ Ex0.java
@@ -0,0 +1,22 @@
+package myMath;
+
+import java.util.ArrayList;
+
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
+public class Ex0 {
+
+	public static void main(String[] args) {
+
+		Polynom p0 = new Polynom("0.2x^4 + -1.5x^3 + 3x^2 + -1x + -5");
+		p0.draw(-2, 6, 100);
+		
+		
+		
+	}
+	
+	
+
+}
